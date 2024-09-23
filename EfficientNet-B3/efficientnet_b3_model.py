7e1a56f8c31f0045c3136fe99d379d7f5c20c7ce
raafayk7 <raafaykazmi@yahoo.com>
2024-09-23 11:40:05 +0500

    Update efficientnet_b3_model.py

--- EfficientNet-B3/efficientnet_b3_model.py
+++ EfficientNet-B3/efficientnet_b3_model.py
@@ -4,15 +4,15 @@ import torch.nn as nn
 
 
 class EfficientNetB3Model(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes, pretrained=True):
         super(EfficientNetB3Model, self).__init__()
-        self.model = timm.create_model('efficientnet_b3', pretrained=True)
+        self.enetb3 = timm.create_model('efficientnet_b3', pretrained=pretrained)
 
     def forward(self, x):
-        x = self.model(x)
+        x = self.enetb3(x)
         return x
 
 def load_efficientnetb3_model(num_classes, device):
-    model = ViTClassifier(num_classes)
+    model = EfficientNetB3Model(num_classes)
     model = model.to(device)
     return model
