ff9f3fb7dcca492b31ad13fd974cab946719d2ba
raafayk7 <raafaykazmi@yahoo.com>
2024-09-23 16:25:04 +0500

    Create inference_efficientnetb3_task2.py

---
+++ EfficientNet-B3/Task 2/inference_efficientnetb3_task2.py
@@ -0,0 +1,35 @@
+import torch
+from efficientnet_b3_model import load_efficientnetb3_model
+from data_cifar10 import get_data_loaders_cifar10
+
+def evaluate_model(model, dataloader, device):
+    model.eval()
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():
+        for inputs, labels in dataloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    return accuracy
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    # Load CIFAR-10 dataset
+    train_loader, test_loader, num_classes = get_data_loaders_cifar10()
+    
+    # Load model
+    model = load_efficientnetb3_model(num_classes, device,task='task2')
+    
+    # Evaluate model on CIFAR-10 test dataset
+    accuracy = evaluate_model(model, test_loader, device)
+    print(f"Accuracy on CIFAR-10 test set: {accuracy:.2f}%")
+
+if __name__ == "__main__":
+    main()
